72decf13f1d2a88f5774a4a671d877d2b4cc7a89
Pawan Kumar <itsmekr.info@gmail.com>
2026-08-30 19:45:55 +0530

    Fix PWA cache updates

--- sw.js
+++ sw.js
@@ -1,1 +1,47 @@
-const C='java1000-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./sw.js']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C = 'java1000-v2';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(C).then(cache =>
+            cache.addAll([
+                './',
+                './index.html',
+                './data.json',
+                './manifest.webmanifest',
+                './sw.js'
+            ])
+        )
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== C)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                const copy = response.clone();
+
+                caches.open(C).then(cache => {
+                    cache.put(event.request, copy);
+                });
+
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
